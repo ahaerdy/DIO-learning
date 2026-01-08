c28ffb05b5743947d402d0d03a7e199a089b417e
Arthur <arthur.haerdy@gmail.com>
2026-01-08 13:12:16 -0300

    feat: adiciona role acessível ao parágrafo e atualiza testes de UI

--- TQI Fullstack Developer/Modulo_04/projeto/naruto-quotes-client/src/pages/app/App.js
+++ TQI Fullstack Developer/Modulo_04/projeto/naruto-quotes-client/src/pages/app/App.js
@@ -3,7 +3,7 @@ import narutoImg from '../../images/naruto.png';
 export function App() {
   return (
     <div>
-      <p>quote xpto – Speaker</p>
+      <p role="p">quote xpto – Speaker</p>
       <button>Quote No Jutsu</button>
       <img src={narutoImg} alt="Naruto with a kunai" />
     </div>
