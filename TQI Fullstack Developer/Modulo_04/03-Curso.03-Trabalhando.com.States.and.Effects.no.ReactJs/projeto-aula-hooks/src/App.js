478b32b29acd2f604a652a1e4d779ac1e873553c
Arthur <arthur.haerdy@gmail.com>
2026-01-09 10:04:10 -0300

    feat: adiciona mídias e mensagem de boas-vindas ao projeto de Hooks

--- TQI Fullstack Developer/Modulo_04/03-Curso.03-Trabalhando.com.States.and.Effects.no.ReactJs/projeto-aula-hooks/src/App.js
+++ TQI Fullstack Developer/Modulo_04/03-Curso.03-Trabalhando.com.States.and.Effects.no.ReactJs/projeto-aula-hooks/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <h4>Olá, seja bem-vindo ao meu projeto React!</h4>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
